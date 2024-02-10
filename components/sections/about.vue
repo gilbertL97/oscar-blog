21499221e4d86839385c7f54344eef5d4b337e19
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-10 18:07:10 -0500

    comenzando el about

--- components/sections/about.vue
+++ components/sections/about.vue
@@ -9,26 +9,19 @@
       <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
         <img
           alt="foto Lment dance"
-          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          src="/images/salto.webp"
           class="absolute inset-0 h-full w-full object-cover"
         />
       </div>
 
       <div class="lg:py-24">
-        <h2 class="text-3xl font-bold sm:text-4xl">Bailarin</h2>
+        <h2 class="text-3xl font-bold sm:text-4xl">{{$t('dancer') }}</h2>
 
         <p class="mt-4 text-gray-600">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
           eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
           quidem quam repellat.
         </p>
-
-        <a
-          href="#"
-          class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-          Get Started Today
-        </a>
       </div>
     </div>
   </div>
