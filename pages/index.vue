db6521afb859b861300ff748a76d425b57082801
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-10 05:54:50 -0500

    comenzando con las secciones

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-  <div class="text-red  bg-gray-300">
-    Hello world
+  <div class=" bg-gray-300 bg-white shadow dark:bg-gray-800">
+    <section class="lg:min-h-3xl flex items-center justify-center">
+<h1 class="text-red text-center text-5xl md:text-7xl items-center justify-center"> Asere  {{ $t('dance') }} </h1>
+    </section>
+    <SectionsAbout class="#About"/>
   </div>
 </template>
