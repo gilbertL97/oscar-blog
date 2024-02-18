08e10c755ede4a7a151de92bcc04ac3320e2786b
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-18 05:00:06 -0500

    agregando la seccion servicios

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,6 @@
   <div class="  bg-white shadow dark:bg-gray-800 text-gray-700 dark:text-gray-200">
     <SectionsIntro/>
     <SectionsAbout class="#About"/>
+    <SectionsServices/>
   </div>
 </template>
