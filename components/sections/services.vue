f151ac778005448bd52bacfc46d83ac79c699384
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-18 06:06:15 -0500

    agregando traduccion en los servicios

--- components/sections/services.vue
+++ components/sections/services.vue
@@ -2,96 +2,37 @@
 <section >
   <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:pb-16 lg:pt-10">
     <div class="mx-auto max-w-lg text-center pb-10">
-      <h2 class="text-3xl font-bold sm:text-4xl">Nuestros Servicios</h2>
+      <h2 class="text-3xl font-bold sm:text-4xl">{{ $t('our_services') }}</h2>
     </div>
 
-    <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+    <div class="mt-8 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
 
+      <a
+        class="block rounded-xl border dark:border-gray-700  p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('dance_clase')}}</h2>
+
+        <p class="mt-1 text-sm ">{{ $t('learn_dance') }} </p>
+      </a>
       <a
         class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+        <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('personalice_choreo')}}</h2>
 
-        <h2 class="mt-4 text-xl font-bold ">Clases de baile</h2>
-
-        <p class="mt-1 text-sm ">
-            Aprende diferentes estilos de baile con nuestros instructores profesionales
+        <p class="mt-1 text-sm pb-4">
+            {{$t('uniqueDanceChoreography')}}
         </p>
       </a>
       <a
         class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 class="mt-4 text-xl font-bold ">Coreografías personalizadas</h2>
+        <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('workshopsAndEvents')}}</h2>
 
         <p class="mt-1 text-sm ">
-            Crea una coreografía única para tu boda, fiesta o evento especial
-        </p>
-      </a>
-      <a
-        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 class="mt-4 text-xl font-bold ">Talleres y eventos</h2>
-
-        <p class="mt-1 text-sm ">
-            Participa en nuestros talleres y eventos para mejorar tus habilidades de baile y conocer a otros bailarines
+           {{ $t('improveDanceSkills') }}
         </p>
       </a>
 
@@ -99,29 +40,10 @@
         class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 class="mt-4 text-xl font-bold ">Presentaciones y shows</h2>
+        <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('performancesAndShows') }}</h2>
 
         <p class="mt-1 text-sm ">
-            Contrata a nuestro equipo para presentaciones y shows en eventos y espectáculos.
+            {{$t('hiringOurTeam') }}
         </p>
       </a>
     </div>
