a25d46009e223388c1a6c5793df5b9f92151e35f
Gilbert Luis <gilbertluis97@gmail.com>
2024-02-19 00:02:18 -0500

    agregando iconos

--- components/sections/services.vue
+++ components/sections/services.vue
@@ -11,38 +11,42 @@
         class="block rounded-xl border dark:border-gray-700  p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
+      <div class="i-material-symbols-light-desktop-mac-outline-rounded size-10 text-pink-500"></div>
         <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('dance_clase')}}</h2>
 
-        <p class="mt-1 text-sm ">{{ $t('learn_dance') }} </p>
+        <p class="mt-1 text-sm text-gray-400">{{ $t('learn_dance') }} </p>
       </a>
       <a
-        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-blue-500/10 hover:shadow-blue-500/10"
         href="#"
       >
+      <div class="i-material-symbols-light-sports-gymnastics-rounded size-10 text-blue-500"></div>
         <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('personalice_choreo')}}</h2>
 
-        <p class="mt-1 text-sm pb-4">
+        <p class="mt-1 text-sm pb-4 text-gray-400">
             {{$t('uniqueDanceChoreography')}}
         </p>
       </a>
       <a
-        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-yellow-500/10 hover:shadow-yellow-500/10"
         href="#"
       >
+      <div class="i-material-symbols-light-data-thresholding-outline size-10 text-yellow-500"></div>
         <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('workshopsAndEvents')}}</h2>
 
-        <p class="mt-1 text-sm ">
+        <p class="mt-1 text-sm text-gray-400">
            {{ $t('improveDanceSkills') }}
         </p>
       </a>
 
       <a
-        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        class="block rounded-xl border dark:border-gray-700 p-8 shadow-xl transition hover:border-green-500/10 hover:shadow-green-500/10"
         href="#"
       >
+      <div class="i-material-symbols-light-mic-external-on-rounded size-10 text-green-500"></div>
         <h2 class="mt-4 mb-2 pb-8 text-xl font-bold ">{{$t('performancesAndShows') }}</h2>
 
-        <p class="mt-1 text-sm ">
+        <p class="mt-1 text-sm text-gray-400">
             {{$t('hiringOurTeam') }}
         </p>
       </a>
